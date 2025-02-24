e50b43ed97b86dd561f6f980904ff6ea5448b529
mukesh7260 <mk2648054@gmail.com>
2025-02-24 12:24:10 +0000

    commit now

--- try.py
+++ try.py
@@ -6,3 +6,16 @@
 #     d[i] = d.get(i,0)+1
 # for k , v in d.items():
 #     print(f'{k} - {v}')
+
+
+s = 'rohit shrama'
+d = {}
+for i in s:
+    if i in ' ':
+        continue 
+    elif i in d:
+        d[i] = d[i]+1
+    else:   
+        d[i] = 1
+for k , v in d.items():
+    print(f'{k} - {v}')
