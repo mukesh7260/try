ab8294a5a352dab70c46626217fb329561d41b0c
mukesh7260 <mk2648054@gmail.com>
2025-02-24 12:20:53 +0000

    comment

--- try.py
+++ try.py
@@ -1,8 +1,8 @@
-s = 'rohit shrama'
-d = {}
-for i in s:
-    if i in " ":
-        continue
-    d[i] = d.get(i,0)+1
-for k , v in d.items():
-    print(f'{k} - {v}')
+# s = 'rohit shrama'
+# d = {}
+# for i in s:
+#     if i in " ":
+#         continue
+#     d[i] = d.get(i,0)+1
+# for k , v in d.items():
+#     print(f'{k} - {v}')
