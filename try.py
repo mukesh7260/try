b6f0735d48f57a0817961088a8c81ea8a7f70e5e
mukesh7260 <mk2648054@gmail.com>
2025-02-24 17:15:43 +0000

    commit

--- try.py
+++ try.py
@@ -16,7 +16,7 @@
 
 
 # n = int(input("enter number of rows :  ")) 
-# for i in range(n-2,-1,-1):
+# for i in range(n-1,-1,-1):
 #     print("  "*(n-i) + " *  "*i) 
 
 
@@ -28,3 +28,61 @@
 #             *   *   *
 #             *   *
 #                 *
+
+
+# n = int(input("enter number of rows :  "))
+# for i in range(n+1):
+#     print("  "*(n-i) + " *  "*i)
+# for i in range(n-1,-1,-1):
+#     print("  "*(n-i) + " *  "*i) 
+
+
+# enter number of rows :  9
+
+#                  *
+#                *   *
+#              *   *   *
+#            *   *   *   *
+#          *   *   *   *   *
+#        *   *   *   *   *   *
+#      *   *   *   *   *   *   *
+#    *   *   *   *   *   *   *   *
+#  *   *   *   *   *   *   *   *   *
+#    *   *   *   *   *   *   *   *
+#      *   *   *   *   *   *   *
+#        *   *   *   *   *   *
+#          *   *   *   *   *
+#            *   *   *   *
+#              *   *   *
+#                *   *
+#                  *
+
+
+# n = int(input("enter number of rows :  "))  
+# for i in range(n+1):
+#     print(" * "*i)
+# for i in range(n-1,-1,-1):
+#     print(" * "*i)
+
+
+# enter number of rows :  9
+
+#  *
+#  *  *
+#  *  *  *
+#  *  *  *  *
+#  *  *  *  *  *
+#  *  *  *  *  *  *
+#  *  *  *  *  *  *  *
+#  *  *  *  *  *  *  *  *
+#  *  *  *  *  *  *  *  *  *
+#  *  *  *  *  *  *  *  *
+#  *  *  *  *  *  *  *
+#  *  *  *  *  *  *
+#  *  *  *  *  *
+#  *  *  *  *
+#  *  *  *
+#  *  *
+#  *
+
+
