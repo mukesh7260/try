170c9b44b9c3e885d9a756ded065e5f5b9011484
mukesh7260 <mk2648054@gmail.com>
2025-02-25 08:09:05 +0000

    commit

--- pratice.py
+++ pratice.py
@@ -10,12 +10,14 @@
 
 
 
-# l = [23,65,45,98,75,35] 
-# for i in range(len(l)):
-#     for j in range(len(l)):
-#         if l[i] > l[j]:
-#             l[i],l[j] = l[j],l[i] 
-# print(l)
+l = [23,65,45,98,75,35] 
+for i in range(len(l)):
+    for j in range(len(l)):
+        if l[i] > l[j]:
+            l[i],l[j] = l[j],l[i] 
+print(l)
+
+
 
         # Diamond shape 
 
