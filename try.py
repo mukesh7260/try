c131599268b04a1cd1b940ce51e714664803b01c
mukesh7260 <mk2648054@gmail.com>
2025-02-25 07:39:03 +0000

    commit

--- try.py
+++ try.py
@@ -1,9 +1,18 @@
 
 # find largest number in the list without using bultin method and function 
 
-l = [23,45,64,34,98,76]
-lmax = l[3]
+# l = [23,45,64,34,98,76]
+# lmax = l[3]
+# for i in range(len(l)):
+#     if l[i] > lmax:
+#         lmax = l[i]
+# print(lmax)
+
+
+
+l = [23,65,45,98,75,35] 
 for i in range(len(l)):
-    if l[i] > lmax:
-        lmax = l[i]
-print(lmax)
+    for j in range(len(l)):
+        if l[i] > l[j]:
+            l[i],l[j] = l[j],l[i] 
+print(l)
