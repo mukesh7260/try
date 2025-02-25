d93637bc17ac627e8714d54fd47972ac6d47a9f5
mukesh7260 <mk2648054@gmail.com>
2025-02-25 07:34:38 +0000

    commit

--- try.py
+++ try.py
@@ -1,88 +1,9 @@
-# n = int(input("enter number of rows :  ")) 
-# for i in range(n+1):
-#     print("  "*(n-i) + " *  "*i)
 
-# o/p : - 
+# find largest number in the list without using bultin method and function 
 
-#                  *
-#                *   *
-#              *   *   *
-#            *   *   *   *
-#          *   *   *   *   *
-#        *   *   *   *   *   *
-#      *   *   *   *   *   *   *
-#    *   *   *   *   *   *   *   *
-#  *   *   *   *   *   *   *   *   *
-
-
-# n = int(input("enter number of rows :  ")) 
-# for i in range(n-1,-1,-1):
-#     print("  "*(n-i) + " *  "*i) 
-
-
-# enter number of rows :  9
-#     *   *   *   *   *   *   *
-#     *   *   *   *   *   *
-#         *   *   *   *   *
-#         *   *   *   *
-#             *   *   *
-#             *   *
-#                 *
-
-
-# n = int(input("enter number of rows :  "))
-# for i in range(n+1):
-#     print("  "*(n-i) + " *  "*i)
-# for i in range(n-1,-1,-1):
-#     print("  "*(n-i) + " *  "*i) 
-
-
-# enter number of rows :  9
-
-#                  *
-#                *   *
-#              *   *   *
-#            *   *   *   *
-#          *   *   *   *   *
-#        *   *   *   *   *   *
-#      *   *   *   *   *   *   *
-#    *   *   *   *   *   *   *   *
-#  *   *   *   *   *   *   *   *   *
-#    *   *   *   *   *   *   *   *
-#      *   *   *   *   *   *   *
-#        *   *   *   *   *   *
-#          *   *   *   *   *
-#            *   *   *   *
-#              *   *   *
-#                *   *
-#                  *
-
-
-# n = int(input("enter number of rows :  "))  
-# for i in range(n+1):
-#     print(" * "*i)
-# for i in range(n-1,-1,-1):
-#     print(" * "*i)
-
-
-# enter number of rows :  9
-
-#  *
-#  *  *
-#  *  *  *
-#  *  *  *  *
-#  *  *  *  *  *
-#  *  *  *  *  *  *
-#  *  *  *  *  *  *  *
-#  *  *  *  *  *  *  *  *
-#  *  *  *  *  *  *  *  *  *
-#  *  *  *  *  *  *  *  *
-#  *  *  *  *  *  *  *
-#  *  *  *  *  *  *
-#  *  *  *  *  *
-#  *  *  *  *
-#  *  *  *
-#  *  *
-#  *
-
-
+l = [23,45,64,34,98,76]
+lmax = l[3]
+for i in range(len(l)):
+    if l[i] > lmax:
+        lmax = l[i]
+print(lmax)
